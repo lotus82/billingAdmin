0053cfb7cca10dda94c3ba1d2ea86106bdfd7fac
lotus82 <39626203+lotus82@users.noreply.github.com>
2019-07-30 20:05:57 +0400

    Add files via upload

---
+++ Readini/Readini/Readini.vb
@@ -0,0 +1,42 @@
+﻿
+Public Class Readini
+    Private Declare Function GetPrivateProfileString Lib "kernel32" Alias "GetPrivateProfileStringA" (ByVal lpAppName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As String, ByVal nSize As Integer, ByVal lpFileName As String) As Integer
+    Private Declare Function WritePrivateProfileString Lib "Kernel32" Alias "WritePrivateProfileStringA" (ByVal Section As String, ByVal Key As String, ByVal putStr As String, ByVal INIfile As String) As Integer
+
+    Public Shared Function ReadIni(ByVal Section As String, ByVal Param As String) As String
+
+        Dim app As String
+        app = My.Application.Info.AssemblyName
+        Dim exit_code As String
+        If FileIO.FileSystem.FileExists(My.Application.Info.DirectoryPath & "\" & app & ".ini") = False Then
+            MsgBox("Не удалось загрузить файл конфигурации", MsgBoxStyle.Critical)
+            Err.Description = "Не удалось загрузить файл конфигурации"
+            Err.Number = 101
+            exit_code = "101"
+
+            Return exit_code
+            Exit Function
+        End If
+        Try
+            Dim rc As String = Strings.StrDup(255, vbNullChar)
+            Dim x As Integer
+
+            x = GetPrivateProfileString(Section, Param, "", rc, 255, My.Application.Info.DirectoryPath & "\" & app & ".ini")
+            If x <> 0 Then rc = Strings.Left(rc, x)
+            Return rc
+            If x = 0 Then WritePrivateProfileString("APP", "APP_GUID", Guid.NewGuid.ToString, My.Application.Info.DirectoryPath & "\" & app & ".ini")
+        Catch ex As Exception
+
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+        Exit Function
+
+    End Function
+    Public Shared Function WriteIni(ByVal Section As String, ByVal Param As String, ByVal value As String) As String
+        Dim app As String
+        app = My.Application.Info.AssemblyName
+        WritePrivateProfileString(Section, Param, value, My.Application.Info.DirectoryPath & "\" & app & ".ini")
+        Return Nothing
+    End Function
+End Class
